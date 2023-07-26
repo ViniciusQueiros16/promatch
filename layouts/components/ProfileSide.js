09d1f69bccf5c8e46efaa3c079b0bf47023ba777
Vinicius <vinny.queiyros16@gmail.com>
2023-07-26 18:03:58 -0300

    Get user data and fix theme responsive in ProfileSide

---
+++ layouts/components/ProfileSide.js
@@ -0,0 +1,79 @@
+import { Avatar } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { BsFillBookmarkStarFill } from "react-icons/bs";
+import ImageFallback from "./ImageFallback";
+
+const ProfileSide = ({ session }) => {
+  const [speed, setSpeed] = useState();
+
+  useEffect(() => {
+    setSpeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="hidden md:col-span-2 xl:inline-grid">
+      <div className="item-center col-span-2 flex flex-col px-2 md:items-start">
+        {/* Top */}
+        <div className="hidden:md relative flex flex-col items-center overflow-hidden rounded-lg border bg-theme-light text-center dark:bg-darkmode-body">
+          <div className="relative ">
+            <ImageFallback
+              className="h-full w-full"
+              height="100"
+              width="200"
+              src="/images/background.jpg"
+            />
+          </div>
+          <Avatar
+            name={session.username}
+            // onClick={() => signOut()}
+            src={
+              session.avatar
+                ? session.avatar
+                : `https://avatars.dicebear.com/api/avataaars/${speed}.svg`
+            }
+            className="!absolute !top-9 !h-14 !w-14 !cursor-pointer !border-2"
+          />
+          <div className="mt-5 space-x-0.5 py-4">
+            <h4 className="cursor-pointer decoration-purple-700 underline-offset-1 hover:underline">
+              {session.username}
+            </h4>
+            <p className="text-sm text-dark dark:text-darkmode-light">
+              {session.email}
+            </p>
+          </div>
+
+          <div className="hidden text-left text-sm text-gray-500 md:inline">
+            <div className="sidebarButton space-y-0.5 font-medium">
+              <div className="flex justify-between space-x-2">
+                <h6>Connection</h6>
+                <span className="text-blue-500">321</span>
+              </div>
+              <div className="flex justify-between space-x-2">
+                <h6>Views of your post</h6>
+                <span className="text-blue-500">1,892</span>
+              </div>
+            </div>
+
+            <div className="sidebarButton">
+              <h4 className="text-xs leading-4">
+                Access exclusive tools & insights
+              </h4>
+              <h6 className="font-light text-dark dark:text-darkmode-light">
+                <span className="mr-1 inline-block h-3 w-3 rounded-sm bg-gradient-to-tr from-yellow-700 to-yellow-200" />
+                Try Premium for free
+              </h6>
+            </div>
+
+            <div className="sidebarButton flex items-center space-x-1.5">
+              <BsFillBookmarkStarFill className="!-ml-1" />
+              <h6 className="font-medium text-dark dark:text-darkmode-light">
+                My items
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProfileSide;

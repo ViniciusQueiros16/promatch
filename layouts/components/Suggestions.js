03d2dbaa75202dd0c48f7be7f1c5e224a21a3e4b
Vinicius <vinny.queiyros16@gmail.com>
2023-07-26 18:39:09 -0300

    Fixed theme responsive in Suggestions

--- layouts/components/Suggestions.js
+++ layouts/components/Suggestions.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import ImageFallback from "./ImageFallback";
-
+import Button from "@layouts/shortcodes/Button";
 
 const Suggestions = () => {
   const [suggestions, setSuggestions] = useState([]);
@@ -23,43 +23,49 @@ const Suggestions = () => {
   }, []);
 
   return (
-    <div className="hidden xl:inline space-y-2">
-      <div className="bg-white py-2.5 rounded-lg space-y-2 w-11/12 overflow-hidden border border-gray-300 dark:border-none m-auto">
-        <div className="flex justify-between text-sm nb-d-5">
-          <h3 className="text-sm font-bold text-gray-900 m-2">
+    <div className="hidden space-y-2 xl:inline">
+      <div className="m-auto w-11/12 space-y-2 overflow-hidden rounded-lg border border-gray-300 bg-theme-light py-2.5 dark:border-none dark:bg-darkmode-body">
+        <div className="nb-d-5 flex justify-between text-sm">
+          <h3 className="m-2 text-sm font-bold text-gray-900">
             Add to your feed
           </h3>
         </div>
         {suggestions.map((profile, key) => (
           <div key={profile.userId}>
-            <div className="flex items-center justify-between m-2 mb-3">
+            <div className="m-2 mb-3 flex items-center justify-between">
               <img
-                className="w-10 h-10
+                className="h-10 w-10
           rounded-full border p-[2px]"
                 src={profile.avatar}
                 alt=""
               />
-              <div className="flex-1 ml-4">
-                <h2 className="font-semibold text-sm">{profile.username}</h2>
+              <div className="ml-4 flex-1">
+                <h2 className="text-sm font-semibold">{profile.username}</h2>
                 <h3 className="text-xs text-gray-400">{profile.company}</h3>
               </div>
             </div>
             <div className="flex justify-center" key={profile.password}>
-              <button className="bg-transparent hover:bg-gray-200 text-gray-600 border border-gray-500  font-bold py-0.5 px-4 rounded-full text-sm -mt-2">
+              <Button
+                href="/contractors"
+                type="outline"
+                className="-mt-2 text-sm "
+              >
                 + Follow
-              </button>
+              </Button>
             </div>
           </div>
         ))}
       </div>
       <hr />
       {/* Ads */}
-      <div className="bg-white w-11/12 h-64 px-2.5 rounded-lg sticky top-20 border border-gray-300 dark:border-none  m-auto">
-        <div className="relative w-full h-full">
-        <ImageFallback
+      <div className="sticky top-20 m-auto h-64 w-11/12 rounded-lg border border-gray-300 bg-theme-light px-2.5 dark:border-none  dark:bg-darkmode-body">
+        <div className="relative h-full w-full">
+          <ImageFallback
             src="/images/trabalhadores.png"
             className=""
-            width={200} height={200}
+            width={300}
+            height={300}
+            alt="Ads"
           />
         </div>
       </div>

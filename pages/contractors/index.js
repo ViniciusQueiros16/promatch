bf17959aa4d93fe8075ada5672856e4f1bf892b6
Vinicius <vinny.queiyros16@gmail.com>
2023-07-26 18:42:22 -0300

    Added contractors session

--- pages/contractors/index.js
+++ pages/contractors/index.js
@@ -1,9 +1,22 @@
 import { getCookie } from "cookies-next";
-import Feed from "@layouts/components/feed";
+import { faker } from "@faker-js/faker"; // Import faker library correctly
+import Feed from "@layouts/components/Feed";
+import ProfileSide from "@layouts/components/ProfileSide";
+import RightHandSide from "@layouts/components/RightHandSide";
+import HomeHeader from "@layouts/components/HomeHeader";
 
-const Contractors = () => {
+const Contractors = ({ session }) => {
   return (
-   <Feed></Feed>
+    <>
+      <HomeHeader />
+      <div className="mx-auto mt-8 max-h-screen overflow-hidden lg:max-w-6xl">
+        <main className="grid grid-cols-9">
+          <ProfileSide session={session} />
+          <Feed />
+          <RightHandSide session={session} />
+        </main>
+      </div>
+    </>
   );
 };
 
@@ -14,8 +27,20 @@ export const getServerSideProps = async ({ req, res }) => {
     const token = getCookie("authorization", { req, res });
     if (!token) throw new Error("Token Invalido!");
 
+    // Generate fake user session data using faker
+    const session = {
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      // company: faker.company.bs(),
+      // id: i,
+    };
+
     return {
-      props: {},
+      props: {
+        session,
+      },
     };
   } catch (error) {
     return {

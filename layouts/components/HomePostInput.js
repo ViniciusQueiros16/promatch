cea7ff5f5fb23066671967e1fc7bb238ae304b4b
Vinicius <vinny.queiyros16@gmail.com>
2023-07-31 19:03:37 -0300

    Fixed HomePostInput component

--- layouts/components/HomePostInput.js
+++ layouts/components/HomePostInput.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 
 import { BsYoutube } from "react-icons/bs";
@@ -8,11 +7,9 @@ import { MdAssignment, MdEventNote } from "react-icons/md";
 // import CreatePost from "./CreatePost";
 import { Avatar, Input } from "@mui/material";
 
-
 const HomePostInput = ({ session }) => {
- 
   const [speed, setSpeed] = useState();
- 
+
   const [isCreatePostOpen, setIsCreatePostOpen] = useState(false);
 
   useEffect(() => {
@@ -28,7 +25,7 @@ const HomePostInput = ({ session }) => {
   };
 
   return (
-    <div className=" bg-theme-light dark:bg-darkmode-body rounded-lg p-3 space-y-3 border border-gray-300 dark:border-none">
+    <div className=" space-y-3 rounded-lg border border-gray-300 bg-theme-light p-3 dark:border-none dark:bg-darkmode-body">
       <div className="flex items-center space-x-2">
         <Avatar
           name={session?.username}
@@ -39,35 +36,39 @@ const HomePostInput = ({ session }) => {
           }
           className="!h-10 !w-10 cursor-pointer"
         />
-        <Input
+
+        <input
+          type="text"
+          class="peer block min-h-[auto] w-full rounded border border-gray-400 bg-white px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+          id="exampleFormControlInput1"
           placeholder="Have a topic that excites you? Post about it"
-          style={{ borderRadius: "20px", cursor: "pointer" }}
           onClick={handleCreatePostOpen}
         />
+
         <>
-        {/* {isCreatePostOpen && (
+          {/* {isCreatePostOpen && (
           <CreatePost onClose={handleCreatePostClose} speed={speed} />
         )} */}
         </>
       </div>
-      <div className="flex items-center flex-wrap gap-4 justify-center md:gap-x-10">
+      <div className="flex flex-wrap items-center justify-center gap-4 md:gap-x-10">
         <button className="inputButton group">
-          <IoMdPhotos className="text-blue-400 text-2xl" />
-          <h4 className="opacity-80 group-hover:opacity-100">Photo</h4>
+          <IoMdPhotos className="text-2xl text-blue-400" />
+          <h5 className="opacity-80 group-hover:opacity-100">Photo</h5>
         </button>
         <button className="inputButton group">
-          <BsYoutube className="text-green-400 text-2xl" />
-          <h4 className="opacity-80 group-hover:opacity-100">Video</h4>
+          <BsYoutube className="text-2xl text-green-400" />
+          <h5 className="opacity-80 group-hover:opacity-100">Video</h5>
         </button>
         <button className="inputButton group">
-          <MdEventNote className="text-yellow-600 text-2xl" />
-          <h4 className="opacity-80 group-hover:opacity-100">Event</h4>
+          <MdEventNote className="text-2xl text-yellow-600" />
+          <h5 className="opacity-80 group-hover:opacity-100">Event</h5>
         </button>
         <button className="inputButton group">
-          <MdAssignment className="text-red-400 text-2xl" />
-          <h4 className="opacity-80 group-hover:opacity-100 whitespace-nowrap">
+          <MdAssignment className="text-2xl text-red-400" />
+          <h5 className="whitespace-nowrap opacity-80 group-hover:opacity-100">
             Write Article
-          </h4>
+          </h5>
         </button>
       </div>
     </div>

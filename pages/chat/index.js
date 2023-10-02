7e660e1002645f83df6697eb96ff1e13d7f35698
Vinicius <vinny.queiyros16@gmail.com>
2023-10-02 19:21:57 -0300

    Created chat page

---
+++ pages/chat/index.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState } from "react";
+import RightHandSide from "@layouts/components/homeFeed/RightHandSide";
+import MatchesDisplay from "@layouts/components/MatchesDisplay";
+import ChatDisplay from "@layouts/components/ChatDisplay";
+import ContractorsLayoutBase from "@layouts/ContractorsLayoutBase";
+import { Avatar } from "@mui/material";
+import { SessionContext } from "context/SessionContext";
+
+const ChatContainer = () => {
+  const session = useContext(SessionContext);
+  console.log(session);
+  const [clickedUser, setClickedUser] = useState(false);
+  const [speed, setSpeed] = useState();
+
+  useEffect(() => {
+    setSpeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <ContractorsLayoutBase>
+      <div className="flex flex-row justify-center gap-x-4">
+        <div className="chat-container">
+          {/* Header */}
+          <div className="m-auto flex h-24 items-center justify-between rounded-t-2xl bg-gradient-to-tr from-yellow-700 to-yellow-200">
+            <div className="profile">
+              <div className="img-container">
+                <Avatar
+                  src={
+                    session?.user?.avatar
+                      ? session.user?.avatar
+                      : `https://avatars.dicebear.com/api/avataaars/${speed}.svg`
+                  }
+                />
+              </div>
+              <h4>{session?.user?.username}</h4>
+            </div>
+          </div>
+
+          {/* Tabs */}
+          <div className="mt-4 flex flex-row justify-center">
+            <button
+              className="mx-auto text-sm"
+              onClick={() => setClickedUser(true)}
+            >
+              Matches
+            </button>
+            <button
+              className="mx-auto text-sm"
+              onClick={() => setClickedUser(false)}
+            >
+              Chat
+            </button>
+          </div>
+
+          {!clickedUser && <MatchesDisplay setClickedUser={setClickedUser} />}
+
+          {clickedUser && <ChatDisplay clickedUser={clickedUser} />}
+        </div>
+        <RightHandSide />
+      </div>
+    </ContractorsLayoutBase>
+  );
+};
+
+export default ChatContainer;
